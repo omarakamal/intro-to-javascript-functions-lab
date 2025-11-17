1268d2c62add18dc95fb85e1bba40f2ef070333d
omarakamal <omaraakamal@gmail.com>
2025-11-17 20:22:38 +0300

    finish lab

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// function generateEmail(name, email){
+//     return name + '@' + email
+// }
+
+const generateEmail = (name,email)=> name + '@' + email
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
